9b2e8b428c8888ca573645b2d19c119746a36be2
Rishi06x <rishiranjith1234@gmail.com>
2025-07-31 19:41:44 +0530

    Added tailwindcss locally

---
+++ Weather/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./src/**/*.{html,js}",
+    "./js/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
